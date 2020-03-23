8d914cc3d0eb42746a91d50e7dc410b09f780355
RodrigoSantos <rmmss@iscte-iul.pt>
2020-03-23 13:44:24 +0000

    teste

--- HelloWorld/src/main/java/ES2_2019_EIC12_15/HelloWorld/App.java
+++ HelloWorld/src/main/java/ES2_2019_EIC12_15/HelloWorld/App.java
@@ -25,8 +25,10 @@ public class App
 	 */
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! 2" );
     }
+    
+    
     
     
     
