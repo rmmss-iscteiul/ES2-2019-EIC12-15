22527c9f940cba7fc33df7686c71db0754251f67
RodrigoSantos <rmmss@iscte-iul.pt>
2020-05-24 21:58:10 +0100

    teste criar jar

--- HelloWorld/src/main/java/ES2_2019_EIC12_15/HelloWorld/App.java
+++ HelloWorld/src/main/java/ES2_2019_EIC12_15/HelloWorld/App.java
@@ -31,7 +31,7 @@ public class App
     	System.out.println( "Hello World! 4" );
     	System.out.println( "Hello World! 5" );
     	System.out.println( "Hello World! 6" );
-    	System.out.println( "Hello World! 78" );
+    	System.out.println( "Hello World! 7" );
     }
     
     
