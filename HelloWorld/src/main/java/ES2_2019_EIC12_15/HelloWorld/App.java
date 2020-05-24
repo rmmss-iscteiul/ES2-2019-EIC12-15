8aeae583a8518673abf2b4cc9b33138d167bfe56
RodrigoSantos <rmmss@iscte-iul.pt>
2020-05-24 21:44:43 +0100

    aaaa

--- HelloWorld/src/main/java/ES2_2019_EIC12_15/HelloWorld/App.java
+++ HelloWorld/src/main/java/ES2_2019_EIC12_15/HelloWorld/App.java
@@ -31,6 +31,7 @@ public class App
     	System.out.println( "Hello World! 4" );
     	System.out.println( "Hello World! 5" );
     	System.out.println( "Hello World! 6" );
+    	System.out.println( "Hello World! 7" );
     }
     
     
